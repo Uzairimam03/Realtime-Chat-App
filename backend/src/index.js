cd84f6a7aa77442e644ad7bb2bed43c1a07dff09
Your Name <you@example.com>
2025-02-11 19:01:14 +0500

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,27 +11,23 @@ import path from "path"
 import { app, server } from "./lib/socket.js"
 
 dotenv.config();
-const PORT = process.env.PORT
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(cookieParser())
-app.use(cors(
-  {
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-  }))
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes)
+app.use("/api/messages", messageRoutes);
 
-app.use(protectRoute);
-app.get('/check', protectRoute, (req, res) => {
-  res.send('This is a protected route');
-});
-
-if(process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
@@ -40,6 +36,6 @@ if(process.env.NODE_ENV==='production'){
 }
 
 server.listen(PORT, () => {
-  console.log("Server is running on PORT:" + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
-})
+});
